0f47a2baa9abd41b12f7c8bedd146593594b87ca
san-kir-k <dfomka@list.ru>
2021-05-01 17:04:21 +0300

    checked

--- main.cpp
+++ main.cpp
@@ -5,32 +5,28 @@
 #include <queue>
 #include <functional>
 
-using TLl = long long;
-static const TLl INF = INT_MAX;
+using TUll = unsigned long long;
+static const TUll INF = LLONG_MAX;
 
-TLl Dijkstra(const std::vector<std::vector<std::pair<int, int> > >& graph, int start, int finish) {
+TUll Dijkstra(const std::vector<std::vector<std::pair<int, int> > >& graph, int start, int finish) {
     int n = graph.size();
 
-    std::priority_queue<std::pair<TLl, int>, std::vector<std::pair<TLl, int> >, std::greater<std::pair<TLl, int> > > pq;
+    std::priority_queue<std::pair<TUll, int>, std::vector<std::pair<TUll, int> >, std::greater<std::pair<TUll, int> > > pq;
     pq.push(std::make_pair(0, start));
 
-    std::vector<TLl> dist(n, INF);
+    std::vector<TUll> dist(n, INF);
     dist[start] = 0;
-
-    std::vector<bool> used(n);
     
     while (!pq.empty()) {
-        std::pair<int, int> top = pq.top();
+        std::pair<TUll, int> top = pq.top();
         int v = top.second;
-        int vd = top.first;
         pq.pop();
-        used[v] = true;
 
         for (int i = 0; i < graph[v].size(); ++i) {
             int to = graph[v][i].first;
-            int tod = graph[v][i].second;
-            if (vd + tod < dist[to]) {
-                dist[to] = vd + tod;
+            int tow = graph[v][i].second;
+            if (dist[v] + tow < dist[to]) {
+                dist[to] = dist[v] + tow;
                 pq.push(std::make_pair(dist[to], to));
             }
         }
@@ -54,7 +50,7 @@ int main() {
         graph[v-1].push_back(std::make_pair(to-1, w)); 
         graph[to-1].push_back(std::make_pair(v-1, w)); 
     }
-    TLl ans = Dijkstra(graph, start - 1, finish - 1);
+    TUll ans = Dijkstra(graph, start - 1, finish - 1);
     ans == INF ? std::cout << "No solution\n" : std::cout << std::to_string(ans) << "\n";
     return 0;
 }
